1d83ec31c15b8e17cc6742f2234ee40262cbf050
learningcloud <learningcloudforcareer@gmail.com>
2026-02-15 12:45:19 +0530

    correction

--- src/dsa/LongestPalindromicSubstring.java
+++ src/dsa/LongestPalindromicSubstring.java
@@ -6,31 +6,33 @@ public class LongestPalindromicSubstring {
         String str = "babcba";
         int n = str.length();
         String result = "";
-        for (int i = 0; i < str.length(); i++) {
-            //process even
-            int start = i,end = i;
-            while(start>0 && end<n && str.charAt(start)==str.charAt(end)){
-                start--;end++;
-            }
-            String temp = str.substring(start++,end--);
-            if(temp.length()>result.length()){
-                result = temp;
-            }
-        }
 
         for (int i = 0; i < n; i++) {
-            int start = i,end = i+1;
-            while(start>0 && end<n && str.charAt(start) == str.charAt(end)){
-                start--;end++;
+
+            // 1️⃣ Odd length palindrome (center = i)
+            int start = i, end = i;
+            while (start >= 0 && end < n && str.charAt(start) == str.charAt(end)) {
+                start--;
+                end++;
             }
-            String temp = str.substring(start+1,end);
-            if(temp.length()>result.length()){
+            String temp = str.substring(start + 1, end);
+            if (temp.length() > result.length()) {
+                result = temp;
+            }
+
+            // 2️⃣ Even length palindrome (center = i, i+1)
+            start = i;
+            end = i + 1;
+            while (start >= 0 && end < n && str.charAt(start) == str.charAt(end)) {
+                start--;
+                end++;
+            }
+            temp = str.substring(start + 1, end);
+            if (temp.length() > result.length()) {
                 result = temp;
             }
         }
 
         System.out.println(result);
     }
-
-
 }
